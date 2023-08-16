6c4edaedf07d67502352a35316efd69cf0c5beb3
juhoon212 <ws5501@naver.com>
2023-08-16 22:58:12 +0900

    initial commit

---
+++ src/main/java/com/fornula/domain/HomeController.java
@@ -0,0 +1,18 @@
+package com.fornula.domain;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class HomeController {
+	
+	@GetMapping("/home")
+	public String home() {
+		log.info("homecontroller = {}", HomeController.class);
+		return "main";
+	}
+}
