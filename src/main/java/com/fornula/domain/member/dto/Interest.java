1fb70a94c2f617e4608014125db7f0c87c454412
수연 <3437ssy@naver.com>
2023-08-18 17:48:34 +0900

    dto위치변경

---
+++ src/main/java/com/fornula/domain/member/dto/Interest.java
@@ -0,0 +1,23 @@
+package com.fornula.domain.member.dto;
+
+import lombok.Data;
+
+/*
+이름       널?       유형            
+-------- -------- ------------- 
+INTEREST_IDX       NOT NULL NUMBER 
+CATEGORY_IDX_ONE   NOT NULL NUMBER 
+CATEGORY_IDX_TWO   NOT NULL NUMBER 
+CATEGORY_IDX_THREE NOT NULL NUMBER 
+MEMBER_IDX         NOT NULL NUMBER 
+*/
+
+@Data
+public class Interest {
+     private int interestIdx;
+     private int categoryIdxOne;
+     private int categoryIdxTwo;
+     private int categoryIdxThree;
+     private int memberIdx;
+   
+}
