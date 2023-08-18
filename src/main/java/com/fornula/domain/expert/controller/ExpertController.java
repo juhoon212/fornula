695f7dd457b8e53249b5353348a0af560e51daec
hayun <hayun@DESKTOP-INCEULD>
2023-08-18 23:42:09 +0900

    에러 수정

--- src/main/java/com/fornula/domain/expert/controller/ExpertController.java
+++ src/main/java/com/fornula/domain/expert/controller/ExpertController.java
@@ -1,5 +1,17 @@
 package com.fornula.domain.expert.controller;
 
-public class ExpertController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class ExpertController {
+	@RequestMapping(value = "/expertinput", method = RequestMethod.GET)
+	public String input() {
+		log.info("ExpertController 클래스의 input() 메소드 실행");
+		return "expert-input";
+	}
 }
