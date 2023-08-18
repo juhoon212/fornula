86e60a2daa42fa63f6c72a5556f791c0c2ce577c
juhoon212 <ws5501@naver.com>
2023-08-18 12:16:40 +0900

    homecontroller

--- src/main/java/com/fornula/domain/HomeController.java
+++ src/main/java/com/fornula/domain/HomeController.java
@@ -10,21 +10,63 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class HomeController {
 	
-	@GetMapping("/home")
-	public String home() {
-		log.info("Homecontroller = {}", HomeController.class);
-		return "main";
-	}
-	
-	@GetMapping("/item")
-	public String item() {
-		log.info("Homecontroller = {}", HomeController.class);
-		return "item-add";
-	}
-	
-	@GetMapping("/test")
-	@ResponseBody
-	public String test() {
-		return "ok";
-	}
+	   @GetMapping("/home")
+	   public String home() {
+	      log.info("Homecontroller = {}", HomeController.class);
+	      return "main";
+	   }
+	   
+	   @GetMapping("/item")
+	   public String item() {
+	      log.info("Homecontroller = {}", HomeController.class);
+	      return "item-add";
+	   }
+	   
+	   @GetMapping("/mypage")
+	   public String mypage() {
+	      log.info("Homecontroller = {}", HomeController.class);
+	      return "my-page";
+	   }
+	   
+	   @GetMapping("/mypage2")
+	   public String mypage2() {
+	      log.info("Homecontroller = {}", HomeController.class);
+	      return "my-page2";
+	   }
+	   
+	   @GetMapping("/items")
+	   public String items() {
+		   return "items";
+	   }
+	   
+
+	   @GetMapping("/itemboard")
+	   public String itemboard() {
+		   return "item-board";
+	   }
+	   
+
+	   @GetMapping("/reviewform")
+	   public String reviews() {
+		  return "review-form";
+	   }
+	   
+	   @GetMapping("/expertinfo")
+	   public String exportInfo() {
+		   return "expert-info";
+	   }
+	   
+	   @GetMapping("/exportjoin")
+	   public String exportJoin() {
+		   return "export-join";
+	   }
+	   
+	   
+	   
+	   @GetMapping("/test")
+	   @ResponseBody
+	   public String test() {
+	      return "ok";
+	   }
+
 }
