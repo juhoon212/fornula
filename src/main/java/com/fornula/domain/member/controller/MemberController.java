da6b948baf47be47cfb972f6946a11a5d8711810
수연 <3437ssy@naver.com>
2023-08-20 16:32:33 +0900

    member controller

--- src/main/java/com/fornula/domain/member/controller/MemberController.java
+++ src/main/java/com/fornula/domain/member/controller/MemberController.java
@@ -1,5 +1,13 @@
 package com.fornula.domain.member.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MemberController {
 	
+	@GetMapping("/home")
+	public String main() {
+		return "main";
+	}
 }
