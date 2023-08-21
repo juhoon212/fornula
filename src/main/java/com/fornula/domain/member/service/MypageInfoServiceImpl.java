87c349a0c6396277a2baf6bcdb761fe3baa74c6d
수연 <3437ssy@naver.com>
2023-08-21 20:36:12 +0900

    mypage info

--- src/main/java/com/fornula/domain/member/service/MypageInfoServiceImpl.java
+++ src/main/java/com/fornula/domain/member/service/MypageInfoServiceImpl.java
@@ -1,5 +1,14 @@
 package com.fornula.domain.member.service;
 
-public class MypageInfoServiceImpl {
+import org.springframework.stereotype.Service;
 
+import com.fornula.domain.member.repository.MypageInfoRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MypageInfoServiceImpl {
+   private final MypageInfoRepository mypageInfoRepository;
+   
 }
