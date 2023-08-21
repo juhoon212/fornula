e66ec04b8392b44f93ab8b1902c47c15a4600dbe
hayunPark95 <yunine95@naver.com>
2023-08-21 11:26:48 +0900

    풀 전 커밋

--- src/main/java/com/fornula/domain/item/controller/ItemController.java
+++ src/main/java/com/fornula/domain/item/controller/ItemController.java
@@ -1,5 +1,31 @@
 package com.fornula.domain.item.controller;
 
-public class ItemController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class ItemController {
+	//상품 등록
+	@RequestMapping(value = "/itemadd")
+	public String itemadd() {
+		log.info("ItemController 클래스의 itemadd() 메소드 실행");
+		return "item-add";
+	}
+	
+	//상품 상세페이지
+	@RequestMapping(value = "/item")
+	public String item() {
+		log.info("ItemController 클래스의 item() 메소드 실행");
+		return "item";
+	}	
+	
+	//상품 게시판
+	@RequestMapping(value = "/itemboard")
+	public String itemboard() {
+		log.info("ItemController 클래스의 itemboard() 메소드 실행");
+		return "item-board";
+	}	
 }
