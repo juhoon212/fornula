34b3da899a7a5b724456298db096c842cbff68e6
수연 <3437ssy@naver.com>
2023-08-28 19:23:18 +0900

    마이페이지 회원관심사 드디어 출력~~~~~~~

--- src/main/java/com/fornula/domain/member/repository/MypageItemRepository.java
+++ src/main/java/com/fornula/domain/member/repository/MypageItemRepository.java
@@ -1,5 +1,6 @@
 package com.fornula.domain.member.repository;
 
+import com.fornula.domain.item.dto.Category;
 import com.fornula.domain.item.dto.Item;
 import com.fornula.domain.item.dto.Purchase;
 import com.fornula.domain.member.dto.mypage.ItemExpert;
