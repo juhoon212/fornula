be9a1faa4a3006817cdb6d10daf5bfa41cd6f5e4
juhoon212 <ws5501@naver.com>
2023-08-30 15:47:37 +0900

    review develop

--- src/main/java/com/fornula/domain/board/controller/BoardController.java
+++ src/main/java/com/fornula/domain/board/controller/BoardController.java
@@ -5,8 +5,12 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fornula.domain.board.dto.ReviewForm;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,6 +19,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class BoardController {
 	
-	
+	@PostMapping("/add")
+	public String addBoard(@ModelAttribute ReviewForm form) {
+		
+		return null;
+	}
 	
 }
