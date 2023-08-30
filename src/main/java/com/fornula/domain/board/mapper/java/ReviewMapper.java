f703be2e137444bfebbaec92496b9c2ec321c8e8
juhoon212 <ws5501@naver.com>
2023-08-30 14:38:57 +0900

    게시판 출력 기능 완료

--- src/main/java/com/fornula/domain/board/mapper/java/ReviewMapper.java
+++ src/main/java/com/fornula/domain/board/mapper/java/ReviewMapper.java
@@ -1,10 +1,11 @@
 package com.fornula.domain.board.mapper.java;
 
 import java.util.List;
+import java.util.Map;
 
 import com.fornula.domain.board.dto.Reviews;
 
 public interface ReviewMapper {
-	List<Reviews> selectReivewList(int itemIdx, int pageNum);
-	int countReview();
+	List<Reviews> selectReview(Map<String, Object> map);
+	int countReview(int itemIdx);
 }
