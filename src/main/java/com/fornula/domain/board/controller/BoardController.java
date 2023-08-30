1332b56e43b5c3602e1b0b8417de3d312709295a
juhoon212 <ws5501@naver.com>
2023-08-30 17:51:13 +0900

    게시판 삽입 완료

--- src/main/java/com/fornula/domain/board/controller/BoardController.java
+++ src/main/java/com/fornula/domain/board/controller/BoardController.java
@@ -2,27 +2,68 @@ package com.fornula.domain.board.controller;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.fornula.domain.board.dto.Review;
 import com.fornula.domain.board.dto.ReviewForm;
+import com.fornula.domain.board.service.ReviewService;
+import com.fornula.domain.item.dto.Purchase;
+import com.fornula.domain.member.dto.Member;
+import com.fornula.domain.util.session.SessionConst;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/board")
 @Slf4j
+@RequiredArgsConstructor
 public class BoardController {
 	
-	@PostMapping("/add")
-	public String addBoard(@ModelAttribute ReviewForm form) {
+	private final ReviewService reviewService;
+	
+	@PostMapping("/add/{itemIdx}")
+	public String addBoard(@ModelAttribute ReviewForm form,
+							HttpSession session,
+							RedirectAttributes redirectAttributes,
+							@PathVariable Integer itemIdx) {
 		
-		return null;
+		Member loginMember = (Member)session.getAttribute(SessionConst.Login_Member);
+		
+		log.info("loginMember 번호 = {}", loginMember.getMemberIdx());
+		
+		Purchase purchase = reviewService.selectPurchase(loginMember.getMemberIdx(), itemIdx);
+		
+		if(purchase == null) {
+			redirectAttributes.addAttribute("status", false);
+			redirectAttributes.addAttribute("itemIdx", itemIdx);
+			return "redirect:/item/{itemIdx}/1";
+		}
+		
+		Review review = new Review();
+		review.setPurchaseIdx(purchase.getPurchaseIdx());
+		review.setContent(form.getContent());
+		
+		int addResult = reviewService.addReview(review);
+		
+		if(addResult == 0) {
+			return "404";
+		}
+		
+		redirectAttributes.addAttribute("itemIdx", itemIdx);
+		
+		return "redirect:/item/{itemIdx}/1";
 	}
 	
 }
--- src/main/java/com/fornula/domain/board/service/ReviewService.java
+++ src/main/java/com/fornula/domain/board/service/ReviewService.java
@@ -2,8 +2,15 @@ package com.fornula.domain.board.service;
 
 import java.util.Map;
 
+import com.fornula.domain.board.dto.Review;
 import com.fornula.domain.board.dto.Reviews;
+import com.fornula.domain.item.dto.Purchase;
 
 public interface ReviewService {
 	Map<String, Object> selectReviews(int pageNum, int itemIdx);
+	
+	int addReview(Review review);
+	
+	Purchase selectPurchase(int memberIdx, int itemIdx);
+	
 }
