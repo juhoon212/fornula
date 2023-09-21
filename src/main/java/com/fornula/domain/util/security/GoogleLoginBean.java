d9fc522c61d7737e4e6ce220a0d40fda4dcba738
wegpa <wegpa@hyemin>
2023-09-21 20:44:39 +0900

    구글 로그인 구현중

---
+++ src/main/java/com/fornula/domain/util/security/GoogleLoginBean.java
@@ -0,0 +1,17 @@
+package com.fornula.domain.util.security;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GoogleLoginBean {
+	/*
+	 * private static final String clientId = "YOUR_CLIENT_ID";//애플리케이션 클라이언트 아이디값";
+	 * private static final String redirectURI =
+	 * URLEncoder.encode("YOUR_CALLBACK_URL", "UTF-8"); private static final
+	 * SecureRandom random = new SecureRandom(); private static final String state =
+	 * new BigInteger(130, random).toString(); private static final String apiURL =
+	 * "https://nid.naver.com/oauth2.0/authorize?response_type=code"; apiURL +=
+	 * "&client_id=" + clientId; apiURL += "&redirect_uri=" + redirectURI; apiURL +=
+	 * "&state=" + state; session.setAttribute("state", state);
+	 */
+}
---
+++ src/main/java/com/fornula/domain/util/security/GoogleLoginController.java
@@ -0,0 +1,13 @@
+package com.fornula.domain.util.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class GoogleLoginController {
+	
+}
